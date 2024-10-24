3de49c32abca482a7f10969a3b38dd215b0229bd
Carlos Leonardo <carlos.leonardo.goes@gmail.com>
2024-10-24 16:41:12 -0300

    Criação do formulário de adicionar/editar contato

--- src/app/componentes/adicionar-contato/adicionar-contato.component.ts
+++ src/app/componentes/adicionar-contato/adicionar-contato.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
-
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-adicionar-contato',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './adicionar-contato.component.html',
-  styleUrl: './adicionar-contato.component.css'
+  styleUrl: './adicionar-contato.component.css',
 })
 export class AdicionarContatoComponent {
-
+  private formBuilder = inject(FormBuilder);
+  formAdicionarContato = this.formBuilder.group({
+    nome: [''],
+    telefone: [''],
+    email: [''],
+    endereco: [''],
+    dataNascimento: [''],
+    observacoes: [''],
+  });
 }
