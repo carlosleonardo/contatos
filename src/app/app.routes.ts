1c33d88f75298ec85808451095e7b168a17702bc
Carlos Leonardo <carlos.leonardo.goes@gmail.com>
2024-10-24 12:58:06 -0300

    Criados os primeiros componentes e adicionadas as rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { PaginaInicialComponent } from './componentes/pagina-inicial/pagina-inicial.component';
+import { ListaContatosComponent } from './componentes/lista-contatos/lista-contatos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: PaginaInicialComponent,
+  },
+  {
+    path: 'lista-contatos',
+    component: ListaContatosComponent,
+  },
+];
