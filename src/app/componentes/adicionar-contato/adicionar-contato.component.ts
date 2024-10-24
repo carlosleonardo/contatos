de6736c497d6d1b472deb51fd251d7bacea3fdd3
Carlos Leonardo <carlos.leonardo.goes@gmail.com>
2024-10-24 14:49:20 -0300

    Adicionado esboço de adiconar contato

---
+++ src/app/componentes/adicionar-contato/adicionar-contato.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-adicionar-contato',
+  standalone: true,
+  imports: [],
+  templateUrl: './adicionar-contato.component.html',
+  styleUrl: './adicionar-contato.component.css'
+})
+export class AdicionarContatoComponent {
+
+}
