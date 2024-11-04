98b7e430fea5c6f526c35f14122d1bfe9580658b
Carlos Leonardo <carlos.leonardo.goes@gmail.com>
2024-11-04 09:27:58 -0300

    Adicionado tratamento para mudar tema da aplicação

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,19 @@
 import { Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { TrocarTemaService } from './servicos/trocar-tema.service';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, NgbDropdownModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
+  mudarTema(tema: string) {
+    this.servicoTrocarTema.trocarTema(tema);
+  }
   servicoTrocarTema = inject(TrocarTemaService);
   temaEscolhido = window.matchMedia('(prefers-color-scheme: dark)').matches
     ? 'dark'
